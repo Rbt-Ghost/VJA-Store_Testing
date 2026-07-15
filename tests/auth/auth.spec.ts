9cab1c0d682c889721730f93b7294ea2f945ea11
Robert <robertnostor@gmail.com>
2026-07-15 11:31:15 +0300

    [C71] Login with Non-existent Account test

--- tests/auth/auth.spec.ts
+++ tests/auth/auth.spec.ts
@@ -53,4 +53,29 @@ test.describe('Authentication', () => {
 
         await expect(page).toHaveURL('/login');
     });
+
+    test('[C71] Login with Non-existent Account', async ({ page }) => {
+        await page.getByRole('link', {name: 'Login'}).click();
+
+        const emailInput = page.getByTestId('login-email-input');
+        const passInput = page.getByTestId('login-password-input');
+        await expect(emailInput).toBeVisible();
+        await expect(passInput).toBeVisible();
+
+        await emailInput.fill('nonExistentAccount@test.com');
+
+        await expect(emailInput).toHaveValue('nonExistentAccount@test.com');
+
+        await passInput.fill('123456');
+
+        await expect(passInput).toHaveValue('123456');
+
+        await page.getByTestId('login-btn').click();
+
+        const errorMessage = page.getByTestId('login-error');
+
+        await expect(errorMessage).toBeVisible();
+
+        await expect(page).toHaveURL('/login');
+    })
 });
