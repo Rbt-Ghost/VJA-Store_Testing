420f26d2eff568359a5c56d9bd5599980e6717bb
Robert <robertnostor@gmail.com>
2026-07-15 16:41:21 +0300

    add C[57] Search Functionality - Valid Keyword test

--- tests/products/products.spec.ts
+++ tests/products/products.spec.ts
@@ -12,6 +12,27 @@ test.describe('Products', () => {
         await page.getByTestId('login-btn').click();
     });
 
-    
+    test('C[57] Search Functionality - Valid Keyword', async ({ page }) => {
+        await test.step('Navigate to the online store homepage.', async() => {
+            await expect(page).toHaveURL('/products');
+        });
+
+        await test.step('Enter a valid product keyword into the search bar.', async() => {
+            await page.getByTestId('search-input').click();
+            await page.getByTestId('search-input').fill('smartphone');
+        });
+
+        await test.step('Verify the displayed products against the search keyword.', async() => {
+            await page.getByTestId('product-link').click();
+
+            await page.getByTestId('product-detail-name').click();
+            await expect(page.getByTestId('product-detail-name')).toContainText(/smartphone/i);
+
+
+
+            await page.getByTestId('product-detail-description').click();
+            await expect(page.getByTestId('product-detail-description')).toContainText(/smartphone/i);
+        });
+    });
 
 });
