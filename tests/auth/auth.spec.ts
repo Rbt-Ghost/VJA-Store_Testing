292e7a1c544cf0900f31c6f521868d84310477e0
Robert <robertnostor@gmail.com>
2026-07-15 10:57:52 +0300

    add auth.spec.ts with the Authentication test suite and  add [C69] Successful User Login test

---
+++ tests/auth/auth.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Authentication', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/login');
+    });
+
+    test('[C69] Successful User Login', async ({ page }) => {
+        await page.getByRole('link', {name: 'Login'}).click();
+
+        const emailInput = page.getByTestId('login-email-input');
+        const passInput = page.getByTestId('login-password-input');
+        await expect(emailInput).toBeVisible();
+        await expect(passInput).toBeVisible();
+
+        await emailInput.fill('e2e@test.com');
+
+        await expect(emailInput).toHaveValue('e2e@test.com');
+
+        await passInput.fill('123456');
+
+        await expect(passInput).toHaveValue('123456');
+
+        await page.getByTestId('login-btn').click();
+
+        await expect(page).not.toHaveURL('/login');
+        await expect(page.getByRole('button', {name: 'Logout'})).toBeVisible();
+    });
+});
