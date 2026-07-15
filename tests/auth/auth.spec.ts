28a3178da59c718426dc0b9dbd92c97a026cc88c
Robert <robertnostor@gmail.com>
2026-07-15 11:18:18 +0300

    add [C70] Login with Incorrect Password test

--- tests/auth/auth.spec.ts
+++ tests/auth/auth.spec.ts
@@ -27,4 +27,30 @@ test.describe('Authentication', () => {
         await expect(page).not.toHaveURL('/login');
         await expect(page.getByRole('button', {name: 'Logout'})).toBeVisible();
     });
+
+    test('[C70] Login with Incorrect Password', async ({ page }) => {
+        await page.getByRole('link', {name: 'Login'}).click();
+
+        const emailInput = page.getByTestId('login-email-input');
+        const passInput = page.getByTestId('login-password-input');
+        await expect(emailInput).toBeVisible();
+        await expect(passInput).toBeVisible();
+
+        await emailInput.fill('e2e@test.com');
+
+        await expect(emailInput).toHaveValue('e2e@test.com');
+
+        await passInput.fill('wrongpassword');
+
+        await expect(passInput).toHaveAttribute('type', 'password');
+        await expect(passInput).toHaveValue('wrongpassword');
+
+        await page.getByTestId('login-btn').click();
+
+        const errorMessage = page.getByTestId('login-error');
+
+        await expect(errorMessage).toBeVisible();
+
+        await expect(page).toHaveURL('/login');
+    });
 });
